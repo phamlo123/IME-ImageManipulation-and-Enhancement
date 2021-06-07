02f69ca65fe8d34ae638438afbe757d7254a2c47
Pham <pham.lo@northeastern.edu>
2021-06-07 02:15:44 -0400

    second

--- src/model/ImageRepresentation/ImageImplPPM.java
+++ src/model/ImageRepresentation/ImageImplPPM.java
@@ -61,17 +61,54 @@ public class ImageImplPPM extends ImageImpl<PPM> {
       List<List<Double>> matrix) {
     for (int row = 1; row < channel.size() - 1; row++) {
       for (int column = 1; column < channel.get(row).size() - 1; column++) {
-        double temp = channel.get(row - 1).get(column - 1) * matrix.get(0).get(0)
-            + channel.get(row - 1).get(column) * matrix.get(0).get(1)
-            + channel.get(row - 1).get(column + 1) * matrix.get(0).get(2)
-            + channel.get(row).get(column - 1) * matrix.get(1).get(0)
-            + channel.get(row).get(column) * matrix.get(1).get(1)
-            + channel.get(row).get(column + 1) * matrix.get(1).get(2)
-            + channel.get(row + 1).get(column - 1) * matrix.get(2).get(0)
-            + channel.get(row + 1).get(column) * matrix.get(2).get(1)
-            + channel.get(row + 1).get(column + 1) * matrix.get(2).get(2);
-
-        int a = (int) Math.round(Math.min(temp, 255));
+        double temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9;
+        try {
+          temp1 = channel.get(row - 1).get(column - 1) * matrix.get(0).get(0);
+        } catch (IndexOutOfBoundsException e) {
+          temp1 = 0;
+        }
+        try {
+          temp2 = channel.get(row - 1).get(column) * matrix.get(0).get(1);
+        } catch (IndexOutOfBoundsException e) {
+          temp2 = 0;
+        }
+        try {
+          temp3 = channel.get(row - 1).get(column + 1) * matrix.get(0).get(2);
+        } catch (IndexOutOfBoundsException e) {
+          temp3 = 0;
+        }
+        try {
+          temp4 = channel.get(row).get(column - 1) * matrix.get(1).get(0);
+        } catch (IndexOutOfBoundsException e) {
+          temp4 = 0;
+        }
+        try {
+          temp5 =channel.get(row).get(column) * matrix.get(1).get(1);
+        } catch (IndexOutOfBoundsException e) {
+          temp5=0;
+        }
+        try {
+          temp6 = channel.get(row).get(column + 1) * matrix.get(1).get(2);
+        }catch (IndexOutOfBoundsException e) {
+          temp6=0;
+        }
+        try {
+          temp7 = channel.get(row + 1).get(column - 1) * matrix.get(2).get(0);
+        } catch (IndexOutOfBoundsException e) {
+          temp7 = 0;
+        }
+        try {
+          temp8 = channel.get(row + 1).get(column) * matrix.get(2).get(1);
+        } catch (IndexOutOfBoundsException e) {
+          temp8 = 0;
+        }
+        try {
+          temp9 = channel.get(row + 1).get(column + 1) * matrix.get(2).get(2);
+        } catch (IndexOutOfBoundsException e) {
+          temp9=0;
+        }
+        double sum = temp1+temp2+temp3+temp4+temp5+temp6+temp7+temp8+temp9;
+        int a = (int) Math.round(Math.min(sum, 255));
         channel.get(row).set(column, a);
       }
     }
