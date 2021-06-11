d0d8a22ce387e1bebaafec143b6c8e23e465747e
Pham <pham.lo@northeastern.edu>
2021-06-10 20:41:35 -0400

    Third

--- test/ImageUtilTest.java
+++ test/ImageUtilTest.java
@@ -1,5 +1,32 @@
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+import java.util.List;
+import model.Coloring;
+import model.ImageRepresentation.PPM;
+import model.ImageRepresentation.util.ImageUtil;
+import org.junit.Test;
+
 public class ImageUtilTest {
+
+  @Test
+  public void testGetList() {
+    PPM ppm = ImageUtil.importImageFile("Koala.ppm");
+    List<List<Integer>> red = ppm.getColorChannel(Coloring.RED);
+    List<List<Integer>> green = ppm.getColorChannel(Coloring.GREEN);
+    List<List<Integer>> blue = ppm.getColorChannel(Coloring.BLUE);
+    List<List<Color>> a = ImageUtil.getLists(red, green, blue);
+    assertEquals(new PPM(a), ppm);
+  }
+
+  @Test
+  public void testGetList3() {
+    PPM ppm = new PPM();
+    List<List<Integer>> red = ppm.getColorChannel(Coloring.RED);
+    List<List<Integer>> green = ppm.getColorChannel(Coloring.GREEN);
+    List<List<Integer>> blue = ppm.getColorChannel(Coloring.BLUE);
+    List<List<Color>> a = ImageUtil.getLists(red, green, blue);
+    assertEquals(new PPM(a), ppm);
+  }
 
 }
