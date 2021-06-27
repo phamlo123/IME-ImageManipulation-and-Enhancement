3f898f3d1f8a745bbb709978175ea08795d018b3
sguiry40 <sguiry40@gmail.com>
2021-06-27 16:38:54 -0400

    yuh

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import controller.ImageController;
+import controller.NewController;
 import controller.SimpleImageController;
+import gui.ImageViewImpl;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import model.imagerepresentation.Image;
 import model.imagerepresentation.multilayers.MultiLayeredImagesOperations;
 import model.imagerepresentation.multilayers.MultiLayers;
 
@@ -19,9 +27,24 @@ public class Main {
    */
   public static void main(String[] args) throws FileNotFoundException {
 
-    MultiLayers multiLayers = new MultiLayeredImagesOperations();
-    ImageController controller = new SimpleImageController(multiLayers,
-        new InputStreamReader(System.in), System.out);
-    controller.processInteractive();
+    MultiLayers images = new MultiLayeredImagesOperations(new ArrayList<>(
+        Arrays.asList(new Image("Koala.jpg"), new Image("Jellyfish.jpg"))));
+    //MultiLayers images = new MultiLayeredImagesOperations();
+    ImageViewImpl view = new ImageViewImpl();
+    NewController controller = new NewController(images, view);
+    view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    view.setVisible(true);
+    try {
+      UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+    } catch (UnsupportedLookAndFeelException e) {
+      // handle exception
+    } catch (ClassNotFoundException e) {
+      // handle exception
+    } catch (InstantiationException e) {
+      // handle exception
+    } catch (IllegalAccessException e) {
+      // handle exception
+    } catch (Exception e) {
+    }
   }
 }
