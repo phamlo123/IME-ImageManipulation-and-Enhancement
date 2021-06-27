c09fe54c2fa391f8236e9fdfc4f644b39586c820
Pham <pham.lo@northeastern.edu>
2021-06-27 16:37:05 -0400

    a

--- src/controller/NewController.java
+++ src/controller/NewController.java
@@ -2,7 +2,7 @@ package controller;
 
 import static java.lang.String.valueOf;
 
-import gui.ImageViewImpl;
+import gui.ImageView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Scanner;
@@ -17,210 +17,272 @@ import model.imagerepresentation.multilayers.MultiLayers;
  */
 public class NewController implements ActionListener, NewControllerInterface {
 
-  private final ImageViewImpl imageViewImpl;
+  private final ImageView view;
   private final MultiLayers model;
 
   /**
    * Creates a new NewController object used for processing images based on User input.
    *
    * @param model              the model used for processing the images
-   * @param imageViewImpl the view used to take user input and display images
+   * @param view the view used to take user input and display images
    * @throws IllegalArgumentException if the parameters are null
    */
-  public NewController(MultiLayers model, ImageViewImpl imageViewImpl)
+  public NewController(MultiLayers model, ImageView view)
       throws IllegalArgumentException {
-    if (model == null || imageViewImpl == null) {
+    if (model == null || view == null) {
       throw new IllegalArgumentException("Cannot pass null parameters");
     }
-    this.imageViewImpl = imageViewImpl;
+    this.view = view;
     this.model = model;
-    imageViewImpl.setListener(this);
-    imageViewImpl.setImage(model.getTopmost());
+    view.setListener(this);
+    view.setImage(model.getTopmost());
     setCurrentDisplay();
   }
 
-  @Override
+
+  /**
+   * Method that delegates to the model the desired functionality based on the press of a button
+   * from the view.
+   *
+   * @param arg0 the action from the view (button pressed)
+   */
   public void actionPerformed(ActionEvent arg0) {
     this.actionHelper(arg0.getActionCommand());
   }
 
-  @Override
+  /**
+   * Helper for actionPerformed that takes the String version of the ActionEvent given to
+   * actionPerformed. This method is public for easier testing.
+   *
+   * @param command the command to be executed.
+   */
   public void actionHelper(String command) {
     switch (command) {
       case "Blur":
         model.blurringImage();
-        imageViewImpl.setImage(model.getTopmost());
-        imageViewImpl.setText("Blur Filtering has been successfully performed");
+        view.setImage(model.getTopmost());
+        view.setText("Blur Filtering has been successfully performed");
         break;
       case "Sharpen":
         model.sharpeningImage();
-        imageViewImpl.setImage(model.getTopmost());
-        imageViewImpl.setText("Sharpen Filtering has been successfully performed");
+        view.setImage(model.getTopmost());
+        view.setText("Sharpen Filtering has been successfully performed");
         break;
       case "Gray":
         model.createMonochrome();
-        imageViewImpl.setImage(model.getTopmost());
-        imageViewImpl.setText("GrayScaling has been successfully performed");
+        view.setImage(model.getTopmost());
+        view.setText("GrayScaling has been successfully performed");
         break;
       case "Sepia":
         model.createSepia();
-        imageViewImpl.setImage(model.getTopmost());
-        imageViewImpl.setText("Sepia Filtering has been successfully performed");
+        view.setImage(model.getTopmost());
+        view.setText("Sepia Filtering has been successfully performed");
         break;
       case "Import":
-        String load = imageViewImpl.getText("Please enter image file name");
+        String load = view.getText("Please enter image file name");
+        if (load.equals("-1")) {
+          return;
+        }
         Scanner loadScanner = new Scanner(load);
-        String load2 = imageViewImpl.getText("Please enter layer index");
+        String load2 = view.getText("Please enter layer index");
+        if (load2.equals("-1")) {
+          return;
+        }
         Scanner loadScanner2 = new Scanner(load2);
+
         try {
           model.loadImages(new Image(loadScanner.next()), Integer.parseInt(loadScanner2.next()));
-          imageViewImpl.setImage(model.getTopmost());
-          imageViewImpl.setText("Import successful");
+          view.setImage(model.getTopmost());
+          view.setText("Import successful");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + ". Please try again.");
+          view.setText(e.getMessage() + ". Please try again.");
           return;
         }
         break;
       case "Import All":
-        String importAll = imageViewImpl.getText("Please enter text file name");
+        String importAll = view.getText("Please enter text file name");
+        if (importAll.equals("-1")) {
+          return;
+        }
         try {
           model.importAll(importAll);
-          imageViewImpl.setImage(model.getTopmost());
-          imageViewImpl.setText("Import All successful");
+          view.setImage(model.getTopmost());
+          view.setText("Import All successful");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + " Please try again.");
+          view.setText(e.getMessage() + " Please try again.");
           return;
         }
         break;
       case "Export":
-        String export = imageViewImpl
+        String export = view
             .getText("Please enter name for this image");
+        if (export.equals("-1")) {
+          return;
+        }
         Scanner exportScanner = new Scanner(export);
-        String export2 = imageViewImpl.getText("Please enter file format");
+        String export2 = view.getText("Please enter file format");
+        if (export2.equals("-1")) {
+          return;
+        }
         Scanner scannerExport2 = new Scanner(export2);
         try {
           model.saveImages(exportScanner.next(), this.toFormat(scannerExport2.next()));
-          imageViewImpl.setText("Export successfully");
+          view.setText("Export successfully");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + " Please try again.");
+          view.setText(e.getMessage() + " Please try again.");
           return;
         }
         break;
       case "Export All":
-        String exportAll = imageViewImpl
+        String exportAll = view
             .getText("Please enter base name for the images");
+        if (exportAll.equals("-1")) {
+          return;
+        }
         Scanner exportAllScanner = new Scanner(exportAll);
 
-        String exportAll2 = imageViewImpl.getText("Please enter file format");
+        String exportAll2 = view.getText("Please enter file format");
+        if (exportAll2.equals("-1")) {
+          return;
+        }
         Scanner scannerExportAll2 = new Scanner(exportAll2);
 
         try {
           model.exportAll(exportAllScanner.next(), this.toFormat(scannerExportAll2.next()));
-          imageViewImpl.setText("Export All successfully");
+          view.setText("Export All successfully");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + " Please try again.");
+          view.setText(e.getMessage() + " Please try again.");
           return;
         }
         break;
       case "Add Layer":
         model.addLayer();
-        imageViewImpl.setText("New layer added successfully");
+        view.setText("New layer added successfully");
         break;
       case "Remove Layer":
-        String remove = imageViewImpl.getText("Please enter a layer index");
+        String remove = view.getText("Please enter a layer index");
+        if (remove.equals("-1")) {
+          return;
+        }
         try {
           model.removeLayer(Integer.parseInt(remove));
-          imageViewImpl.setText("Remove layer" + remove + " successfully");
+          view.setText("Remove layer" + remove + " successfully");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + ". Please try again.");
+          view.setText(e.getMessage() + ". Please try again.");
           return;
         }
         break;
       case "Visible":
-        String visible = imageViewImpl.getText("Please enter a layer index");
+        String visible = view.getText("Please enter a layer index");
+        if (visible.equals("-1")) {
+          return;
+        }
         try {
           model.setInvisibility(Integer.parseInt(visible), true);
-          imageViewImpl.setImage(model.getTopmost());
-          imageViewImpl.setText(visible + "is set to visible");
+          view.setImage(model.getTopmost());
+          view.setText(visible + "is set to visible");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + ". Please try again.");
+          view.setText(e.getMessage() + ". Please try again.");
           return;
         }
         break;
       case "Invisible":
-        String invisible = imageViewImpl.getText("Please enter a layer index");
+        String invisible = view.getText("Please enter a layer index");
+        if (invisible.equals("-1")) {
+          return;
+        }
         try {
           model.setInvisibility(Integer.parseInt(invisible), false);
-          imageViewImpl.setImage(model.getTopmost());
-          imageViewImpl.setText(invisible + "is set to invisible");
+          view.setImage(model.getTopmost());
+          view.setText(invisible + "is set to invisible");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + " Please try again.");
+          view.setText(e.getMessage() + " Please try again.");
           return;
         }
         break;
       case "Set Current":
-        String current = imageViewImpl.getText("Please enter a layer index");
+        String current = view.getText("Please enter a layer index");
+        if (current.equals("-1")) {
+          return;
+        }
         try {
           model.setCurrent(Integer.parseInt(current));
-          imageViewImpl.setText(current + "is set to current");
+          view.setText(current + "is set to current");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + " Please try again.");
+          view.setText(e.getMessage() + " Please try again.");
           return;
         }
         break;
       case "Checker":
-        String checker = imageViewImpl.getText("Please enter a height");
+        String checker = view.getText("Please enter a height");
+        if (checker.equals("-1")) {
+          return;
+        }
         Scanner checkerScanner = new Scanner(checker);
-        String checker2 = imageViewImpl.getText("Please enter a width");
+        String checker2 = view.getText("Please enter a width");
+        if (checker2.equals("-1")) {
+          return;
+        }
         Scanner checkerScanner2 = new Scanner(checker2);
 
         try {
           ImageFormat image = new Image(Integer.parseInt(checkerScanner.next()),
               Integer.parseInt(checkerScanner2.next()));
           model.addLayer();
-          model.loadImages(image, model.getCurrentLayerIndex());
-          imageViewImpl.setImage(model.getTopmost());
-          imageViewImpl.setText("Custom CheckerBoard successfully created");
+          int layer = model.getNumLayers() - 1;
+          model.loadImages(image, layer);
+          view.setImage(model.getTopmost());
+          view.setText("Custom CheckerBoard successfully created");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + " Please try again.");
+          view.setText(e.getMessage() + " Please try again.");
           return;
         }
         break;
       case "Default":
         ImageFormat image = new Image();
         model.addLayer();
-        model.loadImages(image, model.getCurrentLayerIndex());
-        imageViewImpl.setImage(model.getTopmost());
-        imageViewImpl.setText("CheckerBoard successfully created");
+        int layer = model.getNumLayers() - 1;
+        model.loadImages(image, layer);
+        view.setImage(model.getTopmost());
+        view.setText("CheckerBoard successfully created");
         break;
       case "Mosaic":
-        String mosaic = imageViewImpl
+        String mosaic = view
             .getText("Please enter the number of seeds");
+        if (mosaic.equals("-1")) {
+          return;
+        }
         try {
           model.createMosaic(Integer.parseInt(mosaic));
-          imageViewImpl.setImage(model.getTopmost());
-          imageViewImpl.setText("Mosaic Filtering has been successfully performed with " +
+          view.setImage(model.getTopmost());
+          view.setText("Mosaic Filtering has been successfully performed with " +
               mosaic + " seeds");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + " Please try again.");
+          view.setText(e.getMessage() + " Please try again.");
           return;
         }
         break;
       case "Downsize":
-        String downsize = imageViewImpl
+        String downsize = view
             .getText("Please enter desired relative ratio of new height as doubles");
+        if (downsize.equals("-1")) {
+          return;
+        }
         Scanner downsizeScanner = new Scanner(downsize);
-        String downsize2 = imageViewImpl
+        String downsize2 = view
             .getText("Please enter desired relative ratio of new width as doubles");
+        if (downsize2.equals("-1")) {
+          return;
+        }
         Scanner downsizeScanner2 = new Scanner(downsize2);
 
         try {
           model.downSize(Double.parseDouble(downsizeScanner.next()),
               Double.parseDouble(downsizeScanner2.next()));
-          imageViewImpl.setImage(model.getTopmost());
-          imageViewImpl.setText("Downsizing successfully");
+          view.setImage(model.getTopmost());
+          view.setText("Downsizing successfully");
         } catch (IllegalArgumentException e) {
-          imageViewImpl.setText(e.getMessage() + " Please try again.");
+          view.setText(e.getMessage() + " Please try again.");
           return;
         }
         break;
@@ -233,7 +295,7 @@ public class NewController implements ActionListener, NewControllerInterface {
    * layers in the model.
    */
   private void setCurrentDisplay() {
-    imageViewImpl
+    view
         .setCurrentDisplay(valueOf(model.getCurrentLayerIndex()), valueOf(model.getNumLayers()));
   }
 
